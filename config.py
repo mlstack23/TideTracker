2def5dec634e313ec58a784421ea01dcde06745f
mlstack23 <38731375+mlstack23@users.noreply.github.com>
2024-10-27 18:06:43 -0700

    Update config.py
    added specifics

---
+++ config.py
@@ -0,0 +1,15 @@
+# Optional, displayed on top left
+location = 'Port Townsend'
+# NOAA Station Code for tide data
+station_id = 9444900
+
+# For weather data
+# Create Account on openweathermap.com and get API key
+api_key = '9622e602299fc67f30098119739985f1'
+
+# Get LATITUDE and LONGITUDE of location
+latitude = '48.123'
+longitude = '-122.763'
+units = 'imperial'
+
+timezone = "America/Los_Angeles"
